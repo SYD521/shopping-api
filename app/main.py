f3cf99ca28962838e8891bb8d3781dda90857cb5
SamaraPuga <sammypuga87@outlook.com>
2026-08-17 19:40:07 -0500

    [ADD] configurar conexion mySQL - archivos necesarios y registro de rutas

--- app/main.py
+++ app/main.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
 
+from app.api.test_db import router as test_router
+
 app = FastAPI(
     title="Shopping API",
     version="1.0.0"
 )
 
 
+app.include_router(test_router)
+
+
 @app.get("/")
 def home():
+
     return {
         "message": "Shopping API funcionando correctamente"
     }
