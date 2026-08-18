99460143827c676712eaedbb87dafef69ed04150
SamaraPuga <sammypuga87@outlook.com>
2026-08-17 21:55:01 -0500

    [ADD] terminar el modulo categorias

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ import app.models
 from app.api.test_db import router as test_router
 from app.api.users import router as user_router
 from app.api.auth import router as auth_router
+from app.api.categories import router as category_router
 
 app = FastAPI(
 
@@ -16,10 +17,9 @@ app = FastAPI(
 
 
 app.include_router(test_router)
-
 app.include_router(user_router)
-
 app.include_router(auth_router)
+app.include_router(category_router)
 
 
 @app.get("/")
