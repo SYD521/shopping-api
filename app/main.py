f94166bbdb645e533719df2d55fddda869f189bd
SamaraPuga <sammypuga87@outlook.com>
2026-08-17 21:26:41 -0500

    [ADD] terminar el modulo de usuarios

--- app/main.py
+++ app/main.py
@@ -1,19 +1,32 @@
 from fastapi import FastAPI
 
+import app.models
+
 from app.api.test_db import router as test_router
+from app.api.users import router as user_router
+from app.api.auth import router as auth_router
 
 app = FastAPI(
+
     title="Shopping API",
+
     version="1.0.0"
+
 )
 
 
 app.include_router(test_router)
+
+app.include_router(user_router)
+
+app.include_router(auth_router)
 
 
 @app.get("/")
 def home():
 
     return {
+
         "message": "Shopping API funcionando correctamente"
+
     }
